e44f1d793c9433bcf65e30ea76d5c45527de3e68
adhizsrm <adhizsrm@gmail.com>
2025-02-14 10:28:42 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+import { playlistArr } from "./playlist.js";
+const playlistHtml = playlistArr.map(function(el){
+   return `<section class="card">
+    <div class="card-start">
+        <img src="/img/${el.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${el.title}</h4>
+            <p class="card-artist">${el.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `
+});
+document.getElementById('container').innerHTML = playlistHtml;
